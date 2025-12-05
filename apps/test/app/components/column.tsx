6b5de3c4ca0d7be9343b3e584ed32edd3c4ee16f
Hayden Bleasel <hello@haydenbleasel.com>
2025-12-04 21:47:00 -0800

    Update column.tsx

--- apps/test/app/components/column.tsx
+++ apps/test/app/components/column.tsx
@@ -10,6 +10,6 @@ export const Column = ({ title, children }: ColumnProps) => (
     <div className="shrink-0 bg-secondary p-4">
       <p className="font-medium text-sm">{title}</p>
     </div>
-    <div className="flex-1 overflow-y-auto p-4">{children}</div>
+    <div className="flex-1 space-y-4 overflow-y-auto p-4">{children}</div>
   </div>
 );
