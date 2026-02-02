b982e7446cfab62f1e9a0e791a9f4abb29d54264
AKAS <80155432+aakashching@users.noreply.github.com>
2026-02-02 17:50:48 +0530

    Refactor ZoomOutIcon SVG structure

--- packages/streamdown/lib/icons.tsx
+++ packages/streamdown/lib/icons.tsx
@@ -210,19 +210,19 @@ export const ZoomInIcon = (props: IconProps) => (
 );
 
 export const ZoomOutIcon = (props: IconProps) => (
-<svg 
-    color="currentColor"
-    height={16}
-    strokeLinejoin="round"
-    viewBox="0 0 16 16"
-    width={16}
-    {...props}
+  <svg
+      color="currentColor"
+      height={16}
+      strokeLinejoin="round"
+      viewBox="0 0 16 16"
+      width={16}
+      {...props}
   >
-  <path
-    clipRule="evenodd"
-    d="M6.5 2a4.5 4.5 0 0 1 3.515 7.308l2.839 2.839l.064.078a.5.5 0 0 1-.693.693l-.079-.064l-2.838-2.84A4.5 4.5 0 1 1 6.5 2m0 1a3.5 3.5 0 1 0 0 7a3.5 3.5 0 0 0 0-7m1.85 3.01a.5.5 0 0 1 0 .98l-.1.01h-3.5a.5.5 0 0 1 0-1h3.5z"
-    fill="currentColor"
-    fillRule="evenodd"
+    <path
+      clipRule="evenodd"
+      d="M1.5 6.5C1.5 3.73858 3.73858 1.5 6.5 1.5C9.26142 1.5 11.5 3.73858 11.5 6.5C11.5 9.26142 9.26142 11.5 6.5 11.5C3.73858 11.5 1.5 9.26142 1.5 6.5ZM6.5 0C2.91015 0 0 2.91015 0 6.5C0 10.0899 2.91015 13 6.5 13C8.02469 13 9.42677 12.475 10.5353 11.596L13.9697 15.0303L14.5 15.5607L15.5607 14.5L15.0303 13.9697L11.596 10.5353C12.475 9.42677 13 8.02469 13 6.5C13 2.91015 10.0899 0 6.5 0ZM4.125 5.875H4.75H8.25H8.875V7.125H8.25H4.75H4.125V5.875Z"
+      fill="currentColor"
+      fillRule="evenodd"
     />
-</svg>
+  </svg>
 );
