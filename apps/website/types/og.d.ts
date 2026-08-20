cb266f8a4a433814062e7969d9f9252d1556df7a
Farnabaz <farnabaz@gmail.com>
2026-08-20 12:56:28 +0200

    update package files

--- apps/website/types/og.d.ts
+++
@@ -1,9 +0,0 @@
-import "react";
-
-// next/og supports experimental Tailwind via `tw`, but the React HTMLAttributes
-// augmentation is not always picked up during typechecking.
-declare module "react" {
-  interface HTMLAttributes<T> {
-    tw?: string;
-  }
-}
